3ebdb9083e6e5d8603ca4fd2a63919f093774ca0
ajwtf <ajwtf@icloud.com>
2024-07-24 14:20:39 -0500

    first commit

---
+++ backend/docs/basicInfo.js
@@ -0,0 +1,13 @@
+module.exports = {
+  openapi: "3.0.3", // present supported openapi version
+  info: {
+    title: "pern ecommerce", // short title.
+    description: "A REST E-commerce API made with Express and Postgresql.", //  desc.
+    version: "1.0.0", // version number
+    contact: {
+      name: "AJ Southammavong", // your name
+      email: "arnoldsouth@gmail.com", // your email
+      url: "https://github.com/ajwtf", // your website
+    },
+  },
+};
